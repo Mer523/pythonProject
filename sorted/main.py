267c91129695dd09dcf5911a6bc90c21021899e6
Mer523 <Ronin523@yandex.ru>
2023-04-06 20:06:54 +0300

    txt

--- sorted/main.py
+++ sorted/main.py
@@ -1,3 +1,67 @@
-f = open('2.txt')
-f = read ()
+import os
+def glued_file(page_one=None, page_two=None, page_three=None):
+    if page_one or page_two or page_three is None:
+        page_one = '1.txt'
+        page_two = '2.txt'
+        page_three = '3.txt'
+        os.chdir('sorted')
+        final_file = "glued_file.txt"
+        file1_path = os.path.join(os.getcwd(), page_one)
+        file2_path = os.path.join(os.getcwd(), page_two)
+        file3_path = os.path.join(os.getcwd(), page_three)
+        with open(file1_path, 'r', encoding='utf-8') as f1:
+            file1 = f1.readlines()
+        with open(file2_path, 'r', encoding='utf-8') as f2:
+            file2 = f2.readlines()
+        with open(file3_path, 'r', encoding='utf-8') as f3:
+            file3 = f3.readlines()
+        with open(final_file, 'w', encoding='utf-8') as f_total:
 
+            if len(file1) < len(file2) and len(file1) < len(file3):
+                f_total.write(page_one + '\n')
+                f_total.write(str(len(file1)) + '\n')
+                f_total.writelines(file1)
+                f_total.write('\n')
+            elif len(file2) < len(file1) and len(file2) < len(file3):
+                f_total.write(page_two + '\n')
+                f_total.write(str(len(file2)) + '\n')
+                f_total.writelines(file2)
+                f_total.write('\n')
+            elif len(file3) < len(file1) and len(file3) < len(file2):
+                f_total.write(page_three + '\n')
+                f_total.write(str(len(file3)) + '\n')
+                f_total.writelines(file3)
+                f_total.write('\n')
+            if len(file2) > len(file1) > len(file3) or len(file2) < len(file1) < len(
+                    file3):
+                f_total.write(page_one + '\n')
+                f_total.write(str(len(file1)) + '\n')
+                f_total.writelines(file1)
+                f_total.write('\n')
+            elif len(file1) > len(file2) > len(file3) or len(file2) > len(
+                    file1) and len(file2) < len(file3):
+                f_total.write(page_two + '\n')
+                f_total.write(str(len(file2)) + '\n')
+                f_total.writelines(file2)
+                f_total.write('\n')
+            elif len(file1) > len(file3) > len(file2) or len(file3) > len(
+                    file1) and len(file3) < len(file2):
+                f_total.write(page_three + '\n')
+                f_total.write(str(len(file3)) + '\n')
+                f_total.writelines(file3)
+                f_total.write('\n')
+            if len(file1) > len(file2) and len(file1) > len(file3):
+                f_total.write(page_one + '\n')
+                f_total.write(str(len(file1)) + '\n')
+                f_total.writelines(file1)
+            elif len(file2) > len(file1) and len(file2) > len(file3):
+                f_total.write(page_two + '\n')
+                f_total.write(str(len(file2)) + '\n')
+                f_total.writelines(file2)
+            elif len(file3) > len(file1) and len(file3) > len(file2):
+                f_total.write(page_three + '\n')
+                f_total.write(str(len(file3)) + '\n')
+                f_total.writelines(file3)
+
+
+glued_file()
